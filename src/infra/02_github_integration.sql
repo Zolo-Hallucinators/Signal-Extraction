04ea3e334a7addb051bf7fe51e1a8cf3108eda25
Snowflake-Signal-Extraction <aravind.2002.suresh@gmail.com>
2025-09-08 19:19:56 +0000

    updating infra/ files - adding integration setup related files

--- src/infra/02_github_integration.sql
+++ src/infra/02_github_integration.sql
@@ -1,18 +1,34 @@
 // Following documentation from: https://docs.snowflake.com/en/developer-guide/git/git-setting-up#label-git-setup-token
 
+USE ROLE ACCOUNTADMIN;
 USE DATABASE SIGNAL_EXTRACTION_DB;
 USE SCHEMA UTILS;
 
+-- Base to create secrets
 CREATE OR REPLACE SECRET my_git_token_secret
     TYPE = PASSWORD
     USERNAME = 'Snowflake-Signal-Extraction'
     PASSWORD = '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'
     ;
+    
+SHOW SECRETS;
 
+-- Creating git integration
 CREATE OR REPLACE API INTEGRATION github_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/Zolo-Hallucinators/Signal-Extraction.git')
+  API_ALLOWED_PREFIXES = ('https://github.com/Zolo-Hallucinators')
   ALLOWED_AUTHENTICATION_SECRETS = (my_git_token_secret)
   ENABLED = TRUE
   COMMENT = 'Integration to connect git'
   ;
+
+SHOW API INTEGRATIONS;
+SHOW INTEGRATIONS;
+
+-- creating the git repo so that we can view and access in the utils schema.
+CREATE OR REPLACE GIT REPOSITORY SIGNAL_EXTRACTION_REPO
+    API_INTEGRATION = github_integration
+    GIT_CREDENTIALS = my_git_token_secret
+    ORIGIN = 'https://github.com/Zolo-Hallucinators/Signal-Extraction'
+
+-- SHOW GIT REPOSITORIES; -- Didn't work.
---
+++ src/infra/03_external_access_api_integration.sql
@@ -0,0 +1,25 @@
+USE DATABASE SIGNAL_EXTRACTION_DB;
+USE SCHEMA UTILS;
+
+-- Network Rule Creation
+CREATE OR REPLACE NETWORK RULE news_api_network_rule
+    MODE = EGRESS   -- means outbound traffice FROM snowflake to external is allowed.
+    TYPE = HOST_PORT
+    VALUE_LIST = ('newsapi.org')
+
+-- API Key Creation
+CREATE OR REPLACE SECRET news_api_key
+    TYPE = PASSWORD
+    USERNAME = 'News-API-Key'
+    PASSWORD = 'c494f280427646c78473013990b3cd45'
+    ;
+
+-- External Access Integration
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION news_api_integration
+    ALLOWED_NETWORK_RULES = (news_api_network_rule)
+    ALLOWED_AUTHENTICATION_SECRETS = (news_api_key)
+    ENABLED = TRUE
+    COMMENT = 'News API External Access Integration'
+
+-- Can create a UDF for each endpoint to make it easy.
+-- CREATE OR REPLACE FUNCTION GET_NEWS_API_ENDPOINT_EVERYTHING()
